89ba91166286b0e1e5de2ef0c2424224b8ac8bd2
HanGeonHee <han.gh@salin.co.kr>
2025-02-26 23:51:29 +0900

    Joystick Event 추가

--- Assets/Scripts/JoyStick/JoyStick.cs
+++ Assets/Scripts/JoyStick/JoyStick.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 
 public class JoyStick : MonoBehaviour
 {
+    public event Action<Vector2> UpdateMove; 
+    
     private PlayerController controller;
 
     private void Awake()
@@ -32,15 +34,17 @@ public class JoyStick : MonoBehaviour
     private void OnStartMove(InputAction.CallbackContext obj)
     {
         Vector2 pos = obj.ReadValue<Vector2>();
+        UpdateMove?.Invoke(pos);
     }
-    
+
     private void OnUpdateMove(InputAction.CallbackContext obj)
     {
         Vector2 pos = obj.ReadValue<Vector2>();
+        UpdateMove?.Invoke(pos);
     }
-    
+
     private void OnStopMove(InputAction.CallbackContext obj)
     {
-        Debug.Log("Stop Move");
+        UpdateMove?.Invoke(Vector2.zero);
     }
 }
