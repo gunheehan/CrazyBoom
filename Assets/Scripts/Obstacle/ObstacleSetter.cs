295e178410731d1af3113047eac00394b51fa378
HanGeonHee <han.gh@salin.co.kr>
2025-03-02 17:04:15 +0900

    장애물 생성 제외 구역 설졍

--- Assets/Scripts/Obstacle/ObstacleSetter.cs
+++ Assets/Scripts/Obstacle/ObstacleSetter.cs
@@ -1,16 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleSetter : MonoBehaviour
 {
-    private void Start()
-    {
-        CreateObstacleBox();
-    }
     [SerializeField] private GameObject floorObject;
     [SerializeField] private GameObject obstacleContents;
     [SerializeField] private GameObject obstaclePrefab;
 
+    private HashSet<Vector2Int> ignorefloorpos;
     private readonly int MAPSIZE = 10;
+    
+    private void Start()
+    {
+        ignorefloorpos = new HashSet<Vector2Int>();
+        AddEdgeIgnoreArea(0, 0);
+        AddEdgeIgnoreArea(0, MAPSIZE - 3);
+        AddEdgeIgnoreArea(MAPSIZE - 3, 0);
+        AddEdgeIgnoreArea(MAPSIZE - 3, MAPSIZE - 3);
+        
+        CreateObstacleBox();
+    }
+
+    // Player 생성 위치 생성 무시(+모양)
+    private void AddEdgeIgnoreArea(int x, int y)
+    {
+        List<Vector2Int> offsets = new List<Vector2Int>
+        {
+            new Vector2Int(0,1), new Vector2Int(1,0), new Vector2Int(1,1),
+            new Vector2Int(1,2), new Vector2Int(2,1)
+        };
+
+        foreach (var offset in offsets)
+        {
+            int nx = x + offset.x;
+            int ny = y + offset.y;
+            Debug.Log(nx + "/" + ny);
+            if (IsValidPosition(nx, ny))
+                ignorefloorpos.Add(new Vector2Int(nx, ny));
+        }
+    }
+    private bool IsValidPosition(int x, int y)
+    {
+        return x >= 0 && x < MAPSIZE && y >= 0 && y < MAPSIZE;
+    }
     
     public void CreateObstacleBox()
     {
@@ -31,6 +63,10 @@ public class ObstacleSetter : MonoBehaviour
         {
             for (int z = 0; z < MAPSIZE; z++)
             {
+                Vector2Int initpos = new Vector2Int(x, z);
+                if (ignorefloorpos.Contains(initpos))
+                    continue;
+                
                 Vector3 position = new Vector3(
                     localPosition.x + (x * prefabSizeX),
                     1,
