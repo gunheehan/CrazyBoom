9e2b0e95c3f408e6f4e9730a628941c2e2293de9
HanGeonHee <han.gh@salin.co.kr>
2025-03-02 16:36:20 +0900

    ObstacleBox 동적생성 완료

--- Assets/Scripts/Obstacle/ObstacleSetter.cs
+++ Assets/Scripts/Obstacle/ObstacleSetter.cs
@@ -6,31 +6,45 @@ public class ObstacleSetter : MonoBehaviour
     {
         CreateObstacleBox();
     }
-
-    [SerializeField] private GameObject floorContents;
-    [SerializeField] private GameObject floorPrefab;
+    [SerializeField] private GameObject floorObject;
+    [SerializeField] private GameObject obstacleContents;
+    [SerializeField] private GameObject obstaclePrefab;
 
     private readonly int MAPSIZE = 10;
     
     public void CreateObstacleBox()
     {
-        Vector3 startPosition = Vector3.zero;
+        Renderer planeRenderer = floorObject.GetComponent<Renderer>();
+        float planeSizeX = planeRenderer.bounds.size.x;
+        float planeSizeZ = planeRenderer.bounds.size.z;
+
+        Renderer prefabRenderer = obstaclePrefab.GetComponent<Renderer>();
+        float prefabSizeX = prefabRenderer.bounds.size.x;
+        float prefabSizeZ = prefabRenderer.bounds.size.z;
         
+        float startX = - (planeSizeX / 2) + (prefabSizeX / 2);
+        float startZ = (planeSizeZ / 2) - (prefabSizeZ / 2);
+        
+        Vector3 localPosition = floorObject.transform.InverseTransformPoint(new Vector3(startX, 0 ,startZ)); // Start World Pos to Plane LocalPos
+
         for (int x = 0; x < MAPSIZE; x++)
         {
             for (int z = 0; z < MAPSIZE; z++)
             {
-                Vector3 position = startPosition + new Vector3(x, 1, z);
+                Vector3 position = new Vector3(
+                    localPosition.x + (x * prefabSizeX),
+                    1,
+                    localPosition.z - (z * prefabSizeZ)
+                );
                 SetObstacleBox(position);
             }
-        } 
+        }
     }
 
     private void SetObstacleBox(Vector3 position)
     {
-        GameObject obstacleObject = Instantiate(floorPrefab, floorContents.transform);
-        obstacleObject.transform.position = position + new Vector3(position.x, 0, position.z);
-
+        GameObject obstacleObject = Instantiate(obstaclePrefab, obstacleContents.transform);
+        obstacleObject.transform.position = position;
         obstacleObject.name = "ObstacleBox " + position.x + " / " + position.z;
 
         obstacleObject.GetComponent<Renderer>().material.color = Color.blue;
