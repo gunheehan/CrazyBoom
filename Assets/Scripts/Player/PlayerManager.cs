cba90902735b20801694fee4a70e022783414ccc
HanGeonHee <han.gh@salin.co.kr>
2025-03-08 19:45:40 +0900

    폭탄 관리구조 변경(독립 -> Plane 종속)

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private PlayerMove move;
-    [SerializeField] private BombController bomb;
 
     private PlayerStat stat;
     private PlayerController controller;
@@ -13,24 +13,21 @@ public class PlayerManager : MonoBehaviour
         stat = new PlayerStat();
         controller = new PlayerController();
         move.OnUpdateSpeed(stat.GetPlayerSpeed);
-        bomb.OnUpdatePower(stat.GetPlayerPower);
     }
 
     private void OnEnable()
     {
         stat.OnUpdatePlayerStat += info => move.OnUpdateSpeed(info.speed);
-        stat.OnUpdatePlayerStat += info => bomb.OnUpdatePower(info.power);
 
-        controller.Player.Attack.performed += obj => bomb.SetBomb(transform.position);
+        controller.Player.Attack.performed += CreateBomb;
         controller.Player.Attack.Enable();
     }
 
     private void OnDisable()
     {
         stat.OnUpdatePlayerStat -= info => move.OnUpdateSpeed(info.speed); 
-        stat.OnUpdatePlayerStat -= info => bomb.OnUpdatePower(info.power);
         
-        controller.Player.Attack.performed -= obj => bomb.SetBomb(transform.position);
+        controller.Player.Attack.performed -= CreateBomb;
         controller.Player.Attack.Disable();
     }
 
@@ -38,5 +35,16 @@ public class PlayerManager : MonoBehaviour
     {
         gameObject.transform.position = pos;
         gameObject.SetActive(true);
+    }
+
+    private void CreateBomb(InputAction.CallbackContext obj)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            PlaneParts plane = hit.collider.gameObject.GetComponent<PlaneParts>();
+            plane?.SetBomb(stat.GetPlayerPower);
+        }
     }
 }
