cacc33c33eb7e3502550ed91148823cab2a53610
HanGeonHee <han.gh@salin.co.kr>
2025-07-27 02:03:43 +0900

    Plane 동적 생성 완료

--- Assets/Scripts/Obstacle/ObstacleBox.cs
+++ Assets/Scripts/Obstacle/ObstacleBox.cs
@@ -1,32 +1,20 @@
 using System;
 using Unity.Netcode;
-using UnityEngine;
 
 public class ObstacleBox : NetworkBehaviour, IObstacle
 {
-    public event Action OnDestroyBox = null; 
-    private NetworkVariable<int> boxHp = new NetworkVariable<int>(
-        writePerm: NetworkVariableWritePermission.Server
-    );
-
-    private NetworkVariable<bool> isActive = new NetworkVariable<bool>(
-        writePerm: NetworkVariableWritePermission.Server
-    );
+    public event Action OnDestroyBox = null;
+    private int boxHp;
     
     public void SetInitialHp(int hp)
     {
-        Debug.Log("SetObstacleBox");
-        if (!NetworkManager.Singleton.IsServer) return;
-
-        boxHp.Value = hp;
-        isActive.Value = true;
-        gameObject.SetActive(isActive.Value);
+        boxHp = hp;
     }
 
     public void Damage()
     {
-        boxHp.Value--;
-        if (boxHp.Value < 1)
+        boxHp--;
+        if (boxHp < 1)
         {
             OnDestroyBox?.Invoke();
             gameObject.SetActive(false);
--- Assets/Scripts/Plane/PlaneParts.cs
+++ Assets/Scripts/Plane/PlaneParts.cs
@@ -22,14 +22,14 @@ public class PlaneParts : NetworkBehaviour
         bomb.OnExplodeDirectionAction -= particleController.CreateBombParticle;
     }
 
-    public void SetPlane(Vector3 position, bool isneedObstacle)
-    {
-        Debug.Log("Plane Obstacle State : " + isneedObstacle);
-        gameObject.SetActive(true);
-        gameObject.transform.position = position;
-        if (isneedObstacle)
-            SetObstacleBox();
-    }
+    // public void SetPlane(Vector3 position, bool isneedObstacle)
+    // {
+    //     Debug.Log("Plane Obstacle State : " + isneedObstacle);
+    //     gameObject.SetActive(true);
+    //     gameObject.transform.position = position;
+    //     if (isneedObstacle)
+    //         SetObstacleBox();
+    // }
     
     [ServerRpc(RequireOwnership = false)]
     public void SetBombServerRpc(int power, string playerID)
@@ -40,5 +40,50 @@ public class PlaneParts : NetworkBehaviour
     private void SetObstacleBox()
     {
         obstacleItem.SetInitialHp(Random.Range(1, 3)); // Init 값 설정
+    }
+    
+    private NetworkVariable<Vector3> planePosition = new NetworkVariable<Vector3>();
+    private NetworkVariable<bool> isActivated = new NetworkVariable<bool>(
+        false,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server
+        );
+
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("Network Spawn");
+        // 값이 동기화되었을 때 로컬에서 처리
+        planePosition.OnValueChanged += (_, newValue) =>
+        {
+            SetPlane(newValue, isActivated.Value);
+        };
+
+        isActivated.OnValueChanged += (_, newValue) =>
+        {
+            SetPlane(planePosition.Value, newValue);
+        };
+
+        SetPlane(planePosition.Value, isActivated.Value);
+    }
+
+    public void Init(Vector3 pos, bool activateChild)
+    {
+        if (!IsServer) return;
+
+        planePosition.Value = pos;
+        isActivated.Value = activateChild;
+    }
+
+    private void SetPlane(Vector3 pos, bool activateChild)
+    {
+        Debug.Log("set Plane : " + activateChild);
+
+        transform.localPosition = pos;
+
+        if (activateChild)
+        {
+            obstacleItem.SetInitialHp(Random.Range(1, 3)); // Init 값 설정
+            obstacleItem.gameObject.SetActive(true);
+        }
     }
 }
